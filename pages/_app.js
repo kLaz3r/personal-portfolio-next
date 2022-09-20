0683f8e648a3336a3feeed5c305753b5b38c6c36
Stefan Nasturas <klazer220033@gmail.com>
2022-09-20 12:08:42 +0300

    maybe?

--- pages/_app.js
+++ pages/_app.js
@@ -20,18 +20,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charSet='utf-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
-        />
-        <meta name='description' content='description of your project' />
-        <meta name='theme-color' content='#000' />
         <title>Title of the project</title>
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='shortcut icon' href='/favicon.ico' />
-        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
       </Head>
       <ThemeProvider theme={theme}>
         <Script
